d883d95ef475a822acc5d4cb867189662eaf7066
Ganesh <ganesh.alkurn@gmail.com>
2017-08-16 16:38:43 +0530

    update

--- Module.php
+++ Module.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace alkurn\cms;
-use backend\modules\auth\components\Configs;
+use backend\modules\auth\Module as AuthModule;
 /**
  * item module definition class
  */
-class Module extends \yii\base\Module
+class Module extends AuthModule
 {
     /**
      * @inheritdoc
@@ -16,13 +16,13 @@ class Module extends \yii\base\Module
      * @var array
      * @see [[items]]
      */
-    private $_menus = [];
+    protected $_menus = [];
 
     /**
      * @var array
      * @see [[items]]
      */
-    private $_moduleItems = [
+    protected $_moduleItems = [
         'cms-item'        => 'Pages',
         'blog'        => 'Blog',
     ];
@@ -31,12 +31,13 @@ class Module extends \yii\base\Module
      * @var array
      * @see [[items]]
      */
-    private $_normalizeMenus;
+    protected $_normalizeMenus;
 
     /**
      * Nav bar items
      * @var array
      */
+
     public $navbar;
 
     /**
@@ -54,56 +55,5 @@ class Module extends \yii\base\Module
         parent::init();
     }
 
-    /**
-     * Get avalible menu.
-     * @return array
-     */
-    public function getMenus()
-    {
 
-        if ($this->_normalizeMenus === null) {
-            $mid = '/' . $this->getUniqueId() . '/';
-
-            // resolve ads menus
-            $this->_normalizeMenus = [];
-            //$config = components\Configs::instance();
-            $config = Configs::instance();
-            foreach ($this->_moduleItems as $id => $lable) {
-                if ($id !== 'menu' || ($config->db !== null && $config->db->schema->getTableSchema($config->menuTable) !== null)) {
-                    $this->_normalizeMenus[$id] = ['label' => $lable, 'url' => [$mid . $id]];
-                }
-            }
-            foreach (array_keys($this->controllerMap) as $id) {
-                $this->_normalizeMenus[$id] = ['label' => Inflector::humanize($id), 'url' => [$mid . $id]];
-            }
-
-            // user configure menus
-            foreach ($this->_menus as $id => $value) {
-                if (empty($value)) {
-                    unset($this->_normalizeMenus[$id]);
-                } else {
-                    if (is_string($value)) {
-                        $value = [
-                            'label' => $value,
-                        ];
-                    }
-                    $this->_normalizeMenus[$id] = isset($this->_normalizeMenus[$id]) ? array_merge($this->_normalizeMenus[$id], $value) : $value;
-                    if (!isset($this->_normalizeMenus[$id]['url'])) {
-                        $this->_normalizeMenus[$id]['url'] = [$mid . $id];
-                    }
-                }
-            }
-        }
-        return $this->_normalizeMenus;
-    }
-
-    /**
-     * Set or add avalible menu.
-     * @param array $menus
-     */
-    public function setMenus($menus)
-    {
-        $this->_menus = array_merge($this->_menus, $menus);
-        $this->_normalizeMenus = null;
-    }
 }
